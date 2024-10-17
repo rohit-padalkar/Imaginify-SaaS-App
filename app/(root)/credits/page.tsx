4a4bd3e4f3d0d809177b1de16e2a72659b831a08
Rohit Padalkar <rohitpadalkar5s@gmail.com>
2024-10-17 18:46:02 +0530

    image

--- app/(root)/credits/page.tsx
+++ app/(root)/credits/page.tsx
@@ -1,17 +1,7 @@
 import Image from "next/image";
 
 const Credits = () => {
-  return (
-    <div>
-      credit page
-      <Image
-        src="https://wallpapers.com/images/high/dragon-ball-z-super-saiyan-iag3uvuhauns95dx.webp"
-        alt="goku image"
-        width={500}
-        height={500}
-      />
-    </div>
-  );
+  return <div>credit page</div>;
 };
 
 export default Credits;
--- components/ui/shared/Sidebar.tsx
+++ components/ui/shared/Sidebar.tsx
@@ -14,7 +14,12 @@ const Sidebar = () => {
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
         <Link href="/" className="sidebar-logo">
-          <Image src="/logo-text.svg" alt="logo" width={180} height={28} />
+          <Image
+            src="https://github.com/adrianhajdin/ai_saas_app/blob/main/public/assets/images/logo-text.svg"
+            alt="logo"
+            width={180}
+            height={28}
+          />
           <span className="text-xl font-bold"></span>
         </Link>
 
