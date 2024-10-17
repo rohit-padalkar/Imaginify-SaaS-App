cd5ed9406c3b601f93cac676a6e1da1881c92db7
Rohit Padalkar <rohitpadalkar5s@gmail.com>
2024-10-17 15:02:51 +0530

    sidebar

--- components/ui/shared/Sidebar.tsx
+++ components/ui/shared/Sidebar.tsx
@@ -14,12 +14,7 @@ const Sidebar = () => {
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
         <Link href="/" className="sidebar-logo">
-          <Image
-            src="/assets/images/logo-text.png"
-            alt="logo"
-            width={180}
-            height={28}
-          />
+          <Image src="/logo-text.png" alt="logo" width={180} height={28} />
           <span className="text-xl font-bold"></span>
         </Link>
 
