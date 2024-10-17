28f85c42b9d805cc2f5dc064c3071454c16fc6be
Rohit Padalkar <rohitpadalkar5s@gmail.com>
2024-10-17 15:19:06 +0530

    app

--- app/(root)/credits/page.tsx
+++ app/(root)/credits/page.tsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+
 const Credits = () => {
-  return <div>credit page</div>;
+  return (
+    <div>
+      credit page
+      <Image src="/logo-text.png" alt="logo" width={180} height={28} />
+    </div>
+  );
 };
 
 export default Credits;
