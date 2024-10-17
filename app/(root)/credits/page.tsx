0bc81908ed347f37b8d8132ca4e7aa0d809bcf9d
Rohit Padalkar <rohitpadalkar5s@gmail.com>
2024-10-17 18:39:15 +0530

    image

--- app/(root)/credits/page.tsx
+++ app/(root)/credits/page.tsx
@@ -5,11 +5,10 @@ const Credits = () => {
     <div>
       credit page
       <Image
-        src="/goku.jpg"
+        src="https://wallpapers.com/images/high/dragon-ball-z-super-saiyan-iag3uvuhauns95dx.webp"
         alt="goku image"
-        width={50}
-        height={50}
-        unoptimized
+        width={500}
+        height={500}
       />
     </div>
   );
