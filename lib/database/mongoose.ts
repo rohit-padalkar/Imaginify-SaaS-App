fcacd90166157a5c111407fdda38ec5cbd20d2f8
Rohit Padalkar <rohitpadalkar5s@gmail.com>
2024-10-17 22:14:18 +0530

    mongoose

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,26 +7,29 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose
+// Explicit cast for global
+let cached: MongooseConnection = (global as typeof globalThis & { mongoose: MongooseConnection }).mongoose || { conn: null, promise: null };
 
-if(!cached) {
-  cached = (global as any).mongoose = { 
-    conn: null, promise: null 
-  }
+if (!cached) {
+  cached = { conn: null, promise: null };
 }
 
-export const connectToDatabase = async () => {
-  if(cached.conn) return cached.conn;
+export const connectToDatabase = async (): Promise<Mongoose> => {
+  if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if (!MONGODB_URL) {
+    throw new Error('Missing MONGODB_URL');
+  }
 
-  cached.promise = 
-    cached.promise || 
-    mongoose.connect(MONGODB_URL, { 
-      dbName: 'Imaginify', bufferCommands: false 
-    })
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URL, {
+      dbName: 'Imaginify',
+      bufferCommands: false,
+    });
+  }
 
   cached.conn = await cached.promise;
+  (global as typeof globalThis & { mongoose: MongooseConnection }).mongoose = cached;
 
   return cached.conn;
-}
+};
